39d89e5e9e5c29b344ea38cad656ee6014ab0026
Álvaro <amendoma@alumnos.unex.es>
2025-02-27 17:40:01 +0100

    Reproductor de música de fondo para la web completado. Añadido dependencias de router-react para hacer en un futuro la navegación entre páginas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import WebAudio from "./components/WebAudio";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,10 +24,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <WebAudio /> {/* Separación del audio para reproducirlo en todas las páginas */}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,19 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+  'use client';
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  import Image from "next/image";
+
+  export default function Home() {
+    return (
+      <div style={ styles.container }>
+        <h1>Hola</h1>
+      </div>
+    );
+  }
+  const styles = {
+    container:{
+        display: "flex", 
+        justifyContent: "center",
+        alignItems: "center",
+        height: "50vh"
+    }
 }
