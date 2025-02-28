e95cba7a62780c39a055451303eb01c9e6c10aa8
JorgeCarrascoF <jorgecarfacpsico@gmail.com>
2025-02-28 08:49:38 +0100

    Pruebas componente Album

--- src/app/colors.js
+++ src/app/colors.js
@@ -1,6 +1,8 @@
-export const colors = {
+const colors = {
     primary: '#76ABAE',
     secondary: '#EEEEEE',
     tertiary: '#31363F',
     background: '#222831'
 }
+
+export default colors;
--- src/app/components/Album.tsx
+++ src/app/components/Album.tsx
@@ -1,6 +1,21 @@
+import colors from '../colors'
+
 export const Album = () => {
 
-    return <div>
-
+    return <div style={styles.container}>
+        <h1>Album</h1>
     </div>
+}
+
+const styles = {
+    container: {
+        display: "flex",
+        height: '100px',
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+        backgroundColor: colors.primary,
+    }
+
 }
