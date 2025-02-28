a5ca7d0e9c5a9a31a7ae13156c8d3d9d8f30d4db
JorgeCarrascoF <jorgecarfacpsico@gmail.com>
2025-02-28 10:13:28 +0100

    Album v0.1 y Página de artista v0.1

---
+++ src/app/artist/[id]/page.tsx
@@ -0,0 +1,115 @@
+import Image from "next/image";
+import defaultAvatar from "../../../assets/img/defaultAvatar.jpg";
+import { Album } from "@/app/components/Album";
+
+const ArtistPage = async ({ params }: { params: { id: string } }) => {
+  const param = await params;
+  const id = param.id;
+  
+  const artistAlbums = ["Album 1", "Album 2", "Album 3", "Album 4"];
+
+  const artistGeneros = ["Rock", "Pop", "Jazz", "Blues"];
+
+  return (
+    <main style={styles.main}>
+      <h1 style={styles.title}>Perfil de {id}</h1>
+      <div style={styles.artistContainer}>
+        <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
+          <div style={styles.artistImage}>
+            <Image src={defaultAvatar} alt="artist" width={160} height={160} />
+          </div>
+          <div style={styles.artistButtons}>
+            <button>Seguir</button>
+            <button>Compartir</button>
+          </div>
+        </div>
+        <div style={styles.artistInfo}>
+          <span style={styles.artistName}>{id}</span>
+          <span>ciudad</span>
+          <div style={styles.artistGenres}>
+            {artistGeneros.map((genero) => (
+              <span key={genero}>{genero}</span>
+            ))}
+          </div>
+          <p style={styles.artistDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cum
+            consectetur officiis veniam! Maiores repellat enim ipsum rerum. Aut
+            suscipit eius ea alias. Eum, nesciunt vel consequatur dolores
+            doloremque iure? Accusantium saepe cumque quisquam explicabo odio
+            expedita. Commodi ullam minima hic architecto?
+          </p>
+        </div>
+      </div>
+      <h2 style={{ fontSize: 20 }}>Albums</h2>
+      <div style={styles.artistAlbums}>
+        {artistAlbums.map((album) => (
+          <Album key={album} name={album} />
+        ))}
+      </div>
+    </main>
+  );
+};
+
+const styles = {
+  main: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+  },
+  artistContainer: {
+    borderWidth: 2,
+    padding: 10,
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 20,
+    marginTop: 20,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  artistImage: {
+    borderRadius: 100,
+    overflow: "hidden",
+    backgroundColor: "gray",
+  },
+  artistButtons: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    width: "100%",
+  },
+  artistInfo: {
+    width: "60%",
+    padding: 10,
+    gap: 5,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  artistName: {
+    fontSize: 22,
+    marginLeft: -5,
+  },
+  artistGenres: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    fontSize: 14,
+    gap: 10,
+  },
+  artistDescription: {
+    textAlign: "justify",
+    fontSize: 14,
+    color: "gray",
+  },
+  artistAlbums: {
+    marginTop: 10,
+    display: 'grid',
+    gridTemplateColumns: 'repeat(4, 1fr)',
+    gap: 20,
+  }
+};
+
+export default ArtistPage;
--- src/app/components/Album.tsx
+++ src/app/components/Album.tsx
@@ -1,21 +1,32 @@
+import Image from 'next/image'
 import colors from '../colors'
+import defaultAlbum from '../../assets/img/defaultAlbum.jpg'
 
-export const Album = () => {
 
-    return <div style={styles.container}>
-        <h1>Album</h1>
+export const Album = ({name}: {name: string}) => {
+
+    return <div style={styles.albumContainer}>
+        <Image src={defaultAlbum} style={{borderRadius: 10, padding: 0}} width={120} height={120} alt='album'/>
+        <span style={styles.albumName}>{name}</span>
     </div>
 }
 
 const styles = {
-    container: {
+    albumContainer: {
         display: "flex",
-        height: '100px',
+        width: 150,
         flexDirection: "column",
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderRadius: 12,
         alignItems: "center",
         justifyContent: "center",
         color: "white",
         backgroundColor: colors.primary,
+    },
+    albumName: {
+        fontSize: 16,
+        marginTop: 5,
     }
 
 }
